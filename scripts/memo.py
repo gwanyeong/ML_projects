f0e21a893c8351339b5efb0386c751d311e5cd6f
gwanyeong <61793033+gwanyeong@users.noreply.github.com>
2020-08-15 11:07:07 +0900

    Update memo.py

--- scripts/memo.py
+++ scripts/memo.py
@@ -11,3 +11,13 @@ from pymatgen.io.vasp.inputs import Poscar
 
 poscar = Poscar.from_file('CONTCAR')
 poscar.structure
+
+
+
+
+
+del atoms[atoms.numbers == 6]
+del atoms[[atom.index for atom in atoms if atom.symbol == 'C']]
+del atoms[[atom.symbol == 'C' for atom in atoms]]
+
+del surf_2x2[[atom.position[2]/z_param < 0.35 for atom in surf_2x2]]
