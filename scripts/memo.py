13183fae07b97f20763edc70adae48cca76ef56e
gwanyeong <61793033+gwanyeong@users.noreply.github.com>
2020-08-15 09:30:37 +0900

    Update memo.py

--- scripts/memo.py
+++ scripts/memo.py
@@ -3,3 +3,11 @@ from ase.io.xsd import write_xsd
 
 contcar = read_vasp('CONTCAR')
 write_xsd('contcar.xsd', contcar)
+
+from pymatgen import Structure
+struc = Structure.from_file('CONTCAR')
+
+from pymatgen.io.vasp.inputs import Poscar
+
+poscar = Poscar.from_file('CONTCAR')
+poscar.structure
