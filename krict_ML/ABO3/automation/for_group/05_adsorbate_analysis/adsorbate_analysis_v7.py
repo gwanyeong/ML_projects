da564511c80ce54fb3de3d2c9faf632f72ff64af
gwanyeong <61793033+gwanyeong@users.noreply.github.com>
2020-10-28 01:54:05 +0900

    Add files via upload

--- krict_ML/ABO3/automation/for_group/05_adsorbate_analysis/adsorbate_analysis_v7.py
+++ krict_ML/ABO3/automation/for_group/05_adsorbate_analysis/adsorbate_analysis_v7.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Oct 18 16:24:42 2020
+Created on Tue Aug 18 10:25:52 2020
 
 @author: gyjung
 """
@@ -22,6 +22,7 @@ from ase.io.xsd import write_xsd
 from ase.visualize import view
 
 from pymatgen.io.vasp import Vasprun, Oszicar, Outcar
+from pymatgen import Structure
 
 ###############################################################################
 def createFolder(directory):
@@ -64,10 +65,10 @@ df_entries = pd.read_csv('mpid_list_v2.csv')
 createFolder('results')
 
 properties = ['formula',
-              'convg_bare','tot_E_bare','DE_bare','DG_bare','bare_mag','nsteps_bare','time_bare',
-              'convg_OOH','tot_E_OOH','DE_OOH','DG_OOH','OOH_mag','OOH_dmag','nsteps_OOH','time_OOH',
-              'convg_O', 'tot_E_O','DE_O', 'DG_O','O_mag','O_dmag','nsteps_O','time_O',
-              'convg_OH','tot_E_OH','DE_OH','DG_OH','OH_mag','OH_dmag','nsteps_OH','time_OH']
+              'convg_bare','tot_E_bare','DE_bare','DG_bare','bare_mag','nsteps_bare','struc_bare','time_bare',
+              'convg_OOH','tot_E_OOH','DE_OOH','DG_OOH','OOH_mag','OOH_dmag','nsteps_OOH','struc_OOH','time_OOH',
+              'convg_O', 'tot_E_O','DE_O', 'DG_O','O_mag','O_dmag','nsteps_O','struc_O','time_O',
+              'convg_OH','tot_E_OH','DE_OH','DG_OH','OH_mag','OH_dmag','nsteps_OH','struc_OH','time_OH']
 
 magm_properties = ['magm_bare', 'magm_OOH', 'magm_O', 'magm_OH']
 
@@ -75,14 +76,14 @@ magm_properties = ['magm_bare', 'magm_OOH', 'magm_O', 'magm_OH']
 
 
 # insert the number of index for analysis here
-num_ini = 40
+num_ini = 0
 num_fin = 80
 num = num_fin - num_ini
 
 # Determine whether the analysis includes the calc. time and mag from OUTCAR.
-read_OUTCAR = False
+read_OUTCAR = True
 
-with open('results/results_summary_%03d_%03d' % (num_ini+1, num_fin), 'w') as f:     # put AFM, default in the path if required
+with open('results/results_summary_%03d_%03d' % (num_ini+1, num_fin), 'w') as f:     # put AFM, def in the path if required
     start_time = time.time()
    
     df = pd.DataFrame(np.array([['NaN' for i in range(len(properties))] for j in range(num)]),
@@ -95,7 +96,7 @@ with open('results/results_summary_%03d_%03d' % (num_ini+1, num_fin), 'w') as f:
     for idx in range(num_ini, num_fin):
         formula = df_entries['formula'][idx]
         df.formula[idx + 1] = formula
-        createFolder('results/%03d_%s' % (idx + 1.0, formula))  # put AFM, default in the path if required
+        createFolder('results/%03d_%s' % (idx + 1.0, formula))  # put AFM, def in the path if required
         
         try:
             if os.path.exists(os.getcwd() + '/%03d_%s/2nd/surface_np/cont/' % (idx + 1.0, formula)):
@@ -106,6 +107,7 @@ with open('results/results_summary_%03d_%03d' % (num_ini+1, num_fin), 'w') as f:
             bare_opt = read_vasp(file_path + 'CONTCAR')
             v_bare = Vasprun(file_path + 'vasprun.xml')
             E_bare = v_bare.ionic_steps[-1]['e_wo_entrp']
+            struc_bare = Structure.from_file(file_path + 'CONTCAR')
 
             f.writelines('%03d_%s_bare\n' % (idx + 1.0, formula))
             f.writelines('\tElectronic & ionic converged?: %s\n' % v_bare.converged) 
@@ -116,6 +118,7 @@ with open('results/results_summary_%03d_%03d' % (num_ini+1, num_fin), 'w') as f:
             df.DE_bare[idx + 1] = 0
             df.DG_bare[idx + 1] = 0
             df.nsteps_bare[idx + 1] = len(v_bare.ionic_steps)
+            df.struc_bare[idx + 1] = bare_opt # struc_bare - Pymatgen Structure object
             
             oszicar = Oszicar(file_path + 'OSZICAR')
             mag_bare = oszicar.ionic_steps[-1]['mag']
@@ -146,8 +149,7 @@ with open('results/results_summary_%03d_%03d' % (num_ini+1, num_fin), 'w') as f:
 
                 df_mag.magm_bare[idx+1] = str(metal_magm)
             
-                
-            write_xsd('results/%03d_%s/bare.xsd' % (idx + 1.0, formula), bare_opt)   # put AFM, default in the path if required
+            write_xsd('results/%03d_%s/bare.xsd' % (idx + 1.0, formula), bare_opt)   # put AFM, def in the path if required
           # view(bare_opt)
  
             """
@@ -165,13 +167,16 @@ with open('results/results_summary_%03d_%03d' % (num_ini+1, num_fin), 'w') as f:
                             file_path_ads = file_path + '%s_np/cont/2nd/' % adsorbates[k]
                         else:
                             file_path_ads = file_path + '%s_np/cont/' % adsorbates[k]
+                    elif os.path.exists(file_path + '%s_np/opt/' % adsorbates[k]):
+                        file_path_ads = file_path + '%s_np/opt/' % adsorbates[k]
                     else:
                         file_path_ads = file_path + '%s/' % adsorbates[k]
                     print(file_path_ads)
                    
-                    slab_ini = read_vasp(file_path_ads + 'POSCAR')    # put AFM, default in the path if required
-                    slab_opt = read_vasp(file_path_ads + 'CONTCAR')   # put AFM, default in the path if required
-                    v_slab = Vasprun(file_path_ads + 'vasprun.xml')   # put AFM, default in the path if required
+                    slab_ini = read_vasp(file_path_ads + 'POSCAR')    # put AFM, def in the path if required
+                    slab_opt = read_vasp(file_path_ads + 'CONTCAR')   # put AFM, def in the path if required
+                    v_slab = Vasprun(file_path_ads + 'vasprun.xml')   # put AFM, def in the path if required
+                    struc_opt = Structure.from_file(file_path_ads + 'CONTCAR')    # put AFM, def in the path if required
 
                     E_slab = v_slab.ionic_steps[-1]['e_wo_entrp']
                     DE_ads = E_slab - E_bare - ads_corr[adsorbates[k]]      
@@ -185,24 +190,25 @@ with open('results/results_summary_%03d_%03d' % (num_ini+1, num_fin), 'w') as f:
 
                     # new indexing for iloc func.
                     idx_i = idx - num_ini
-                    df.iloc[idx_i][8*k + 8] = v_slab.converged
-                    df.iloc[idx_i][8*k + 9] = float('%4.4f' % E_slab)
-                    df.iloc[idx_i][8*k + 10] = float('%4.3f' % DE_ads)
-                    df.iloc[idx_i][8*k + 11] = float('%4.3f' % DG_ads)
+                    df.iloc[idx_i][9*k + 9] = v_slab.converged
+                    df.iloc[idx_i][9*k + 10] = float('%4.4f' % E_slab)
+                    df.iloc[idx_i][9*k + 11] = float('%4.3f' % DE_ads)
+                    df.iloc[idx_i][9*k + 12] = float('%4.3f' % DG_ads)
 
-                    oszicar = Oszicar(file_path_ads + 'OSZICAR')      # put AFM, default in the path if required
+                    oszicar = Oszicar(file_path_ads + 'OSZICAR')      # put AFM, def in the path if required
                     mag_ads = oszicar.ionic_steps[-1]['mag']
                     dmag = mag_ads - mag_bare
                     
                     f.writelines('\tmagnetization: %4.3f\n' % mag_ads)
-                    df.iloc[idx_i][8*k + 12] = float('%4.3f' % mag_ads)
-                    df.iloc[idx_i][8*k + 13] = float('%4.3f' % dmag)
-                    df.iloc[idx_i][8*k + 14] = len(v_slab.ionic_steps)
+                    df.iloc[idx_i][9*k + 13] = float('%4.3f' % mag_ads)
+                    df.iloc[idx_i][9*k + 14] = float('%4.3f' % dmag)
+                    df.iloc[idx_i][9*k + 15] = len(v_slab.ionic_steps)
+                    df.iloc[idx_i][9*k + 16] = slab_opt                # struc_opt (pymatgen Structure object)
  
                     if read_OUTCAR:
-                        outcar_ads = Outcar(file_path_ads + 'OUTCAR')      # put AFM, default in the path if required
+                        outcar_ads = Outcar(file_path_ads + 'OUTCAR')      # put AFM, def in the path if required
                         time_ads = float(outcar_ads.run_stats['Total CPU time used (sec)'])/3600.0
-                        df.iloc[idx_i][8*k + 15] = float('%4.3f' % time_ads)
+                        df.iloc[idx_i][9*k + 17] = float('%4.3f' % time_ads)
 
                         z_param = slab_opt.get_cell_lengths_and_angles()[2]
 
@@ -224,23 +230,26 @@ with open('results/results_summary_%03d_%03d' % (num_ini+1, num_fin), 'w') as f:
                     
 #                   view(slab_ini)
 #                   view(slab_opt)
-
-                    print(df)
-                    # put AFM, default in the path if required
+                    if idx < 10:
+                        print(df.head(10))
+                    else:
+                        print(df[idx-10:idx])
+                    # put AFM, def in the path if required
                     write_xsd('results/%03d_%s/%s_opt.xsd' % (idx + 1.0, formula, adsorbates[k]), slab_opt)        
                   
                 except:
-                    f.writelines('%03d_%s_%s files are not found !!\n' % (idx + 1.0, formula, adsorbates[k]))                          
+                    f.writelines('%03d_%s_%s files are not found !!\n' % (idx + 1.0, formula, adsorbates[k]))
+            f.writelines(['#'*80,'\n'])
         except:
             f.writelines('%03d_%s files are not found !!\n' % (idx + 1.0, formula))
             continue    
 
-    df.to_csv('results/df_summary_%03d_%03d.csv' % (num_ini+1, num_fin))     # put AFM, default in the path if required
-    df.to_pickle('results/df_summary_%03d_%03d.pkl' % (num_ini+1, num_fin))  # put AFM, default in the path if required
+    df.to_csv('results/df_summary_%03d_%03d.csv' % (num_ini+1, num_fin))     # put AFM, def in the path if required
+    df.to_pickle('results/df_summary_%03d_%03d.pkl' % (num_ini+1, num_fin))  # put AFM, def in the path if required
 
     if read_OUTCAR:
-        df_mag.to_csv('results/df_mag_%03d_%03d.csv' % (num_ini+1, num_fin))     # put AFM, default in the path if required
-        df_mag.to_pickle('results/df_mag_%03d_%03d.pkl' % (num_ini+1, num_fin))  # put AFM, default in the path if required
+        df_mag.to_csv('results/df_mag_%03d_%03d.csv' % (num_ini+1, num_fin))     # put AFM, def in the path if required
+        df_mag.to_pickle('results/df_mag_%03d_%03d.pkl' % (num_ini+1, num_fin))  # put AFM, def in the path if required
 
     end_time = time.time()
     f.writelines('Execution time for script (sec) : %6.1f\n' % (end_time - start_time))
